5b5c30732a96d2edda773e23e16ebf861506d782
SebastianZal <zalewski.sebastian1@gmail.com>
2018-12-25 01:34:38 +0100

    Result Extensions - change using

--- WebApp.Web/Infrastructure/ResultExtensions.cs
+++ WebApp.Web/Infrastructure/ResultExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.ModelBinding;
+using System.Web.Mvc;
 using WebApp.Logic;
 
 namespace WebApp.Web.Infrastructure
