146e6a48c9a20cf58063392f672962429c05b474
SebastianZal <zalewski.sebastian1@gmail.com>
2018-12-26 04:01:43 +0100

    Autofac configuration

---
+++ WebApp.Web/App_Start/AutofacModules1/ControlerModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+
+namespace AppName.Web.App_Start.AutofacModules
+{
+    public class ControlerModule : Module
+    {
+        protected  override  void Load (ContainerBuilder builder)
+        {
+            builder.RegisterControllers(typeof(ControlerModule).Assembly);
+        }
+    }
+}
